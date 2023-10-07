837a90fbf62df486a6bf382a8663570460386be4
A93MP <agusmore4@gmail.com>
2023-10-07 19:22:08 +0200

    Apollo server setup started

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("@apollo/server");
 const core_1 = require("@mikro-orm/core");
 const express_1 = __importDefault(require("express"));
+const type_graphql_1 = require("type-graphql");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const hello_1 = require("./resolvers/hello");
 const PORT = process.env.PORT || 3000;
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     const em = orm.em.fork();
     await orm.getMigrator().up();
     const app = (0, express_1.default)();
-    app.get('/', (_, res) => {
-        res.send('Hello World from express');
+    const apolloServer = new server_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver],
+            validate: false
+        })
     });
     app.listen(PORT, () => {
         console.log(`server started on http://127.0.0.1:${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+import { ApolloServer } from '@apollo/server'
 import { MikroORM } from '@mikro-orm/core'
 import express from 'express'
+import { buildSchema } from 'type-graphql'
 import mikroOrmConfig from './mikro-orm.config'
+import { HelloResolver } from './resolvers/hello'
 const PORT = process.env.PORT || 3000
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig)
   const em = orm.em.fork()
   await orm.getMigrator().up()
   const app = express()
-  app.get('/', (_, res) => {
-    res.send('Hello World from express')
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
   })
   app.listen(PORT, () => {
     console.log(`server started on http://127.0.0.1:${PORT}`)
